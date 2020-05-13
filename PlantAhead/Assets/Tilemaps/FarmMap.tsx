180eb4b61dea6173b493836a29789a3739983b99
DMChinn <35546158+DMChinn@users.noreply.github.com>
2020-05-13 00:13:19 -0700

    Project set up
    we'll see if the gitignore works. I've started on art and there's a basic tilemap in the scene. Don't worry about the "holes' they'll fill in if you actually start the scene

---
+++ PlantAhead/Assets/Tilemaps/FarmMap.tsx
@@ -0,0 +1,109 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="FarmMap" tilewidth="16" tileheight="16" tilecount="1024" columns="32">
+ <image source="Simple Farm Packv12.png" width="512" height="512"/>
+ <terraintypes>
+  <terrain name="grass" tile="928"/>
+  <terrain name="fencing" tile="928"/>
+ </terraintypes>
+ <tile id="0" terrain="1,1,1,"/>
+ <tile id="1" terrain="1,1,,"/>
+ <tile id="2" terrain="1,1,,"/>
+ <tile id="3" terrain="1,1,,1"/>
+ <tile id="32" terrain="1,,1,"/>
+ <tile id="35" terrain=",1,,1"/>
+ <tile id="36">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="7.95652" width="15.913" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="37">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0434783" y="8" width="15.8696" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="64" terrain="1,,1,1"/>
+ <tile id="65" terrain=",,1,1"/>
+ <tile id="66" terrain=",,1,1"/>
+ <tile id="67" terrain=",1,1,1"/>
+ <tile id="68">
+  <objectgroup draworder="index" id="3">
+   <object id="4" x="0.0268614" y="0" width="15.9825" height="16.0094"/>
+  </objectgroup>
+ </tile>
+ <tile id="69">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0268614" y="0.0805842" width="15.902" height="15.8482"/>
+  </objectgroup>
+ </tile>
+ <tile id="70">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0434783" y="0.0434783" width="15.9565" height="15.913"/>
+  </objectgroup>
+ </tile>
+ <tile id="71">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.0434783" y="0.0434783" width="15.9565" height="15.8696"/>
+  </objectgroup>
+ </tile>
+ <tile id="444">
+  <animation>
+   <frame tileid="444" duration="200"/>
+   <frame tileid="443" duration="400"/>
+  </animation>
+ </tile>
+ <tile id="673" terrain="0,0,0,0"/>
+ <tile id="674" terrain="0,0,0,"/>
+ <tile id="675" terrain="0,0,,"/>
+ <tile id="676" terrain="0,0,,0"/>
+ <tile id="677" terrain=",,,0"/>
+ <tile id="678" terrain=",,0,"/>
+ <tile id="704" terrain="0,0,0,0"/>
+ <tile id="705" terrain="0,0,0,0"/>
+ <tile id="706" terrain="0,,0,"/>
+ <tile id="708" terrain=",0,,0"/>
+ <tile id="709" terrain=",0,,"/>
+ <tile id="710" terrain="0,,,"/>
+ <tile id="736" terrain="0,0,0,0"/>
+ <tile id="737" terrain="0,0,0,0"/>
+ <tile id="738" terrain="0,,0,0"/>
+ <tile id="739" terrain=",,0,0"/>
+ <tile id="740" terrain=",0,0,0"/>
+ <tile id="741">
+  <animation>
+   <frame tileid="704" duration="1000"/>
+   <frame tileid="741" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="742">
+  <animation>
+   <frame tileid="673" duration="1000"/>
+   <frame tileid="742" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="773">
+  <animation>
+   <frame tileid="773" duration="1000"/>
+   <frame tileid="736" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="774">
+  <animation>
+   <frame tileid="705" duration="1000"/>
+   <frame tileid="774" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="776">
+  <animation>
+   <frame tileid="712" duration="2000"/>
+   <frame tileid="745" duration="200"/>
+   <frame tileid="714" duration="50"/>
+  </animation>
+ </tile>
+ <tile id="778">
+  <animation>
+   <frame tileid="712" duration="3010"/>
+   <frame tileid="745" duration="199"/>
+   <frame tileid="714" duration="50"/>
+  </animation>
+ </tile>
+</tileset>

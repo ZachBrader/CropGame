cb64c32f62677854131d65fdb6f589b149b4c9f7
DMChinn <35546158+DMChinn@users.noreply.github.com>
2020-05-23 22:18:25 -0700

    water files

---
+++ PlantAhead/Assets/Scripts/Grid/WaterTile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterTile : CustomTile
+{
+    public Water waterSource;
+
+    public WaterTile(Water water)
+    {
+        waterSource = water;
+    }
+
+    public int refillCan(int waterAmount)
+    {
+        return waterSource.getWater(waterAmount);
+    }
+    
+}

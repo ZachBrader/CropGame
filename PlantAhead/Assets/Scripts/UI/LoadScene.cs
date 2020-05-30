b307bc51c44037bd0e1e0c4516ec2999c2124b2b
Kieran <kierancoito@gmail.com>
2020-05-30 15:20:01 -0700

    added main menu functionality for level select, start game, and instructions

--- PlantAhead/Assets/Scripts/UI/LoadScene.cs
+++ PlantAhead/Assets/Scripts/UI/LoadScene.cs
@@ -1,14 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadScene : MonoBehaviour
-{
+public class LoadScene : MonoBehaviour{
+
+    public Transform main;
+    public Transform levelSelect;
+
+
+    private void Start(){
+        main.gameObject.SetActive(true);
+        levelSelect.gameObject.SetActive(false);
+    }
 
     public void SceneLoader(int SceneIndex)
     {
         SceneManager.LoadScene(SceneIndex);
 
+    }
+
+    public void SubMenuToggle(){
+        if (levelSelect.gameObject.activeInHierarchy == false)
+        {
+            levelSelect.gameObject.SetActive(true);
+            main.gameObject.SetActive(false);
+        }
+        else
+        {
+            levelSelect.gameObject.SetActive(false);
+            main.gameObject.SetActive(true);
+        }
+        
     }
 }
